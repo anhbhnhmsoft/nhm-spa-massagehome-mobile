a40ff4af7ad0967ab7eb91d69f82b130da3048d3
las-tonbd <162237060+Tonph51291@users.noreply.github.com>
2026-03-17 10:26:22 +0700

    fix

--- app/(app)/(customer)/(service)/chat.tsx
+++ app/(app)/(customer)/(service)/chat.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ChatViewScreen from '@/components/app/chat';
-import { useAuthStore } from '@/features/auth/stores';
 
 export default function ChatScreen() {
   return <ChatViewScreen useFor={'customer'} />;
