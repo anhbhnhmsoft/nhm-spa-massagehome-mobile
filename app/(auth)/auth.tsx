3c6d543b2c4d140918a5a507d2146eb10339d5e4
las-tonbd <162237060+Tonph51291@users.noreply.github.com>
2026-03-18 15:50:30 +0700

    TonBD fix : UI auth  choose email , phone number

--- app/(auth)/auth.tsx
+++ app/(auth)/auth.tsx
@@ -1,23 +1,28 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import {
   Keyboard,
   KeyboardAvoidingView,
-  Platform, TextInput, TouchableOpacity,
+  Platform,
+  TextInput,
+  TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {Text} from '@/components/ui/text';
+import { Text } from '@/components/ui/text';
 import { cn } from '@/lib/utils';
 import { Controller } from 'react-hook-form';
 import { useHandleAuthenticate } from '@/features/auth/hooks';
 import { FormInput } from '@/components/ui/form-input';
+import { AtSign, ChevronRightIcon, PhoneIcon } from 'lucide-react-native';
+import { useState } from 'react';
 
 export default function AuthScreen() {
+  const { t } = useTranslation();
 
-  const {t} = useTranslation();
+  const { form, onSubmit, loading } = useHandleAuthenticate();
 
-  const {form, onSubmit, loading} = useHandleAuthenticate();
+  const [show, setShow] = useState(true);
 
   const {
     control,
@@ -28,59 +33,97 @@ export default function AuthScreen() {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView className="flex-1 bg-white">
-        <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          keyboardVerticalOffset={Platform.OS === 'ios' ? 50 : 0}
-          className="flex-1 px-5 pt-4 justify-between"
-        >
-          {/* --- CONTENT --- */}
-          <View>
-            {/* Title */}
-            <Text className="text-2xl font-inter-bold text-gray-900 mb-2">
-              {t('auth.auth_title')}
+        {show ? (
+          <View className="flex-1 bg-white px-4 pt-12">
+            {/* Header */}
+            <Text className="mb-2 font-inter-bold text-2xl tracking-tight text-gray-900">
+              {t('common.welcome_back')}
             </Text>
-            <Text className="text-gray-500 text-base mb-8 leading-6">
-              {t('auth.auth_description')}
+            <Text className="mb-10 text-base leading-6 text-gray-500">
+              {t('common.choose_method')}
             </Text>
 
-            {/* Input Container */}
-            <Controller
-              control={control}
-              name="phone"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <FormInput
-                  required
-                  label={t('common.phone')}
-                  placeholder={t('auth.placeholder_phone')}
-                  keyboardType="number-pad"
-                  autoFocus={true}
-                  value={value}
-                  onChangeText={onChange}
-                  onBlur={onBlur}
-                  error={errors?.phone?.message}
-                />
-              )}
-            />
-          </View>
-
-          {/* --- FOOTER BUTTON --- */}
-          <View className="mb-6">
             <TouchableOpacity
-              onPress={handleSubmit(onSubmit)}
-              disabled={loading} // Disable khi đang loading
-              className={cn(
-                "w-full py-4 rounded-full items-center justify-center",
-                !loading
-                  ? "bg-primary-color-2" // Màu xanh theme Glow (Active)
-                  : "bg-gray-300" // Màu xám (Disabled)
-              )}
-            >
-              <Text className="text-white text-lg font-inter-bold">
-                {loading ? t('common.loading') : t('common.continue')}
-              </Text>
+              activeOpacity={0.7}
+              className="mb-4 flex-row items-center rounded-[24px] border border-gray-100 bg-white p-4"
+              onPress={() => setShow(false)}>
+              <View className="mr-4 h-14 w-14 items-center justify-center rounded-2xl bg-base-color-3">
+                <PhoneIcon size={24} color="#044984" />
+              </View>
+              <View className="flex-1">
+                <Text className="text-[17px] font-semibold text-gray-900">
+                  {t('common.login_phone')}
+                </Text>
+              </View>
+              <ChevronRightIcon />
+            </TouchableOpacity>
+
+            {/* Email Option */}
+            <TouchableOpacity
+              activeOpacity={0.7}
+              className="mb-10 flex-row items-center rounded-[24px] border border-gray-100 bg-white p-4">
+              <View className="mr-4 h-14 w-14 items-center justify-center rounded-2xl bg-base-color-3">
+                <AtSign size={24} color="#044984" />
+              </View>
+              <View className="flex-1">
+                <Text className="text-[17px] font-semibold text-gray-900">Email</Text>
+              </View>
+              <ChevronRightIcon />
             </TouchableOpacity>
           </View>
-        </KeyboardAvoidingView>
+        ) : (
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 50 : 0}
+            className="flex-1 justify-between px-5 pt-4">
+            {/* --- CONTENT --- */}
+            <View>
+              {/* Title */}
+              <Text className="mb-2 font-inter-bold text-2xl text-gray-900">
+                {t('auth.auth_title')}
+              </Text>
+              <Text className="mb-8 text-base leading-6 text-gray-500">
+                {t('auth.auth_description')}
+              </Text>
+
+              {/* Input Container */}
+              <Controller
+                control={control}
+                name="phone"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <FormInput
+                    required
+                    label={t('common.phone')}
+                    placeholder={t('auth.placeholder_phone')}
+                    keyboardType="number-pad"
+                    autoFocus={true}
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    error={errors?.phone?.message}
+                  />
+                )}
+              />
+            </View>
+
+            {/* --- FOOTER BUTTON --- */}
+            <View className="mb-6">
+              <TouchableOpacity
+                onPress={handleSubmit(onSubmit)}
+                disabled={loading} // Disable khi đang loading
+                className={cn(
+                  'w-full items-center justify-center rounded-full py-4',
+                  !loading
+                    ? 'bg-primary-color-2' // Màu xanh theme Glow (Active)
+                    : 'bg-gray-300' // Màu xám (Disabled)
+                )}>
+                <Text className="font-inter-bold text-lg text-white">
+                  {loading ? t('common.loading') : t('common.continue')}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        )}
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
